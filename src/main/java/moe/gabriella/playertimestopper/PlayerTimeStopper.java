7cc403d2a20201093ddaa1f53c200269cabeabcb
Gabriella <isgabriellacurious@gmail.com>
2020-12-28 20:22:21 +0000

    v1.1
    - fixed it not working when you quit
    - verbose config option
    - added it so it turns of ddc if plugin enabled and noone online on start up

--- src/main/java/moe/gabriella/playertimestopper/PlayerTimeStopper.java
+++ src/main/java/moe/gabriella/playertimestopper/PlayerTimeStopper.java
@@ -9,45 +9,67 @@ import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.util.List;
+
 public class PlayerTimeStopper extends JavaPlugin implements Listener {
 
     FileConfiguration config;
+    boolean verbose;
 
     @Override
     public void onEnable() {
+        this.saveDefaultConfig();
+
         config = this.getConfig();
 
         Util.info("Starting PTS...");
 
+        verbose = config.getBoolean("verbose");
+
         if (config.getBoolean("enabled")) {
             this.getServer().getPluginManager().registerEvents(this, this);
             Util.info("Registered events! Plugin is enabled.");
+            toggleTime(!(this.getServer().getOnlinePlayers().size() == 0), true);
         } else {
-            Util.info("Plugin is disabled in config. To enable, set the \"enabled\" value to \"true\"!");
+            Util.info("Plugin is disabled in config. To enable, set the \"enabled\" value to \"true\". All worlds in config have had their DDC's to true.");
+            toggleTime(true, false);
         }
+    }
+
+    @Override
+    public void onDisable() {
+        Util.info("Thank you for using PTS, goodbye!");
     }
 
     @EventHandler
     public void onJoin(PlayerJoinEvent event) {
-        toggleTime(true);
+        toggleTime(true, this.getServer().getOnlinePlayers().size() == 1);
     }
 
     @EventHandler
     public void onQuit(PlayerQuitEvent event) {
-        if (this.getServer().getOnlinePlayers().size() == 0) {
-            toggleTime(false);
+        if ((this.getServer().getOnlinePlayers().size() -1) == 0) {
+            toggleTime(false, true);
         }
     }
 
-    private void toggleTime(boolean result) {
-        for (String s : config.getStringList("worlds")) {
+    private void toggleTime(boolean result, boolean output) {
+        List<String> worlds = config.getStringList("worlds");
+        int failed = 0;
+        for (String s : worlds) {
             World w = this.getServer().getWorld(s);
             if (w == null) {
                 Util.error("World " + s + " does not exist! Please check your config file.");
+                failed++;
                 continue;
             }
 
             w.setGameRule(GameRule.DO_DAYLIGHT_CYCLE, result);
+        }
+        if (output && verbose) {
+            Util.verbose("Do daylight cycle is now " + result + " in " + (worlds.size() - failed) + " world(s).");
+            if (failed > 0)
+                Util.error("Operation failed in " + failed + " world(s).");
         }
     }
 }
--- src/main/java/moe/gabriella/playertimestopper/Util.java
+++ src/main/java/moe/gabriella/playertimestopper/Util.java
@@ -6,6 +6,7 @@ import org.bukkit.ChatColor;
 public class Util {
 
     public static void info(String msg) { Bukkit.getServer().getConsoleSender().sendMessage("" + ChatColor.WHITE + "[PlayerTimeStopper] " + msg); }
+    public static void verbose(String msg) { Bukkit.getServer().getConsoleSender().sendMessage("" + ChatColor.YELLOW + "[PlayerTimeStopper: VERBOSE] " + msg); }
     public static void error(String msg) { Bukkit.getServer().getConsoleSender().sendMessage("" + ChatColor.RED + "[PlayerTimeStopper: ERROR] " + msg); }
 
 }
